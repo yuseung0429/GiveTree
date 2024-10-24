553d871005eeeb5bac3d6333a20477f4c31cbbdd
= <kimdw524@naver.com>
2024-10-25 03:18:16 +0900

    fe/chore: next downgrade
    next 15 -> 14 downgrade

--- frontend/givetree/next.config.mjs
+++ frontend/givetree/next.config.mjs
@@ -1,11 +1,8 @@
-import type { NextConfig } from 'next';
-
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default withVanillaExtract(nextConfig);
