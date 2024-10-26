3b3b5a944bb7e384fba00b4dc9cc5ae598e1fe8e
= <kimdw524@naver.com>
2024-10-27 03:36:12 +0900

    fe/feat: Flex component 추가

---
+++ frontend/givetree/src/components/common/Flex/Flex.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Flex from '.';
+
+const meta = {
+  title: 'UI/Flex',
+  component: Flex,
+
+  parameters: {
+    layout: 'fullscreen',
+  },
+
+  decorators: [
+    (Story) => (
+      <div style={{ padding: '1rem' }}>
+        <Story />
+      </div>
+    ),
+  ],
+
+  tags: ['autodocs'],
+
+  argTypes: {
+    children: {
+      table: { disable: true },
+    },
+  },
+} satisfies Meta<typeof Flex>;
+
+export default meta;
+
+type Story = StoryObj<typeof Flex>;
+
+export const Primary: Story = {
+  args: {
+    children: (
+      <>
+        <div
+          style={{
+            width: '4rem',
+            height: '4rem',
+            background: 'red',
+          }}
+        ></div>
+        <div
+          style={{
+            width: '3rem',
+            height: '3rem',
+            background: 'green',
+          }}
+        ></div>
+        <div
+          style={{
+            width: '2rem',
+            height: '2rem',
+            background: 'blue',
+          }}
+        ></div>
+      </>
+    ),
+  },
+};
