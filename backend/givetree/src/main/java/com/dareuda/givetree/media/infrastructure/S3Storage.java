ad0b89240433f30144f3613901f51fce801c0168
권용진 <dydwls9703@naver.com>
2024-10-30 22:42:04 +0900

    be/fix: multipartFile에서 확장자 얻도록 수정
    #9

--- backend/givetree/src/main/java/com/dareuda/givetree/media/infrastructure/S3Storage.java
+++ backend/givetree/src/main/java/com/dareuda/givetree/media/infrastructure/S3Storage.java
@@ -4,7 +4,6 @@ import io.awspring.cloud.s3.ObjectMetadata;
 import io.awspring.cloud.s3.S3Operations;
 import io.awspring.cloud.s3.S3Resource;
 import lombok.RequiredArgsConstructor;
-import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,13 +19,12 @@ public class S3Storage {
     private String bucket;
 
     private final S3Operations s3Operations;
-    private static final Tika tika = new Tika();
 
     public String upload(MultipartFile multipartFile, String fileName) {
         try (InputStream is = multipartFile.getResource().getInputStream()) {
             S3Resource s3Resource = s3Operations.upload(bucket, fileName, is,
                     ObjectMetadata.builder()
-                            .contentType(tika.detect(multipartFile.getBytes()))
+                            .contentType(multipartFile.getContentType())
                             .build()
             );
             return s3Resource.getURL().toString();
