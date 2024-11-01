4e5ec2cff2f87fe84146063181f3745f7307a904
kimdw524 <kimdw524@naver.com>
2024-11-01 13:35:01 +0900

    fe/feat: scrollbar 디자인

--- frontend/givetree/src/styles/global.css.ts
+++ frontend/givetree/src/styles/global.css.ts
@@ -1,12 +1,27 @@
 import { globalStyle } from '@vanilla-extract/css';
 
 import colorPalette from '@/styles/tokens/colorPalette';
+import color from '@/styles/tokens/color';
 
 globalStyle('*', {
   margin: '0',
   fontFamily: 'inherit',
   WebkitTapHighlightColor: 'transparent',
   boxSizing: 'border-box',
+});
+
+globalStyle('*::-webkit-scrollbar', {
+  width: '0.25rem',
+  height: '0.25rem',
+});
+
+globalStyle('*::-webkit-scrollbar-thumb', {
+  borderRadius: '0.75rem',
+  backgroundColor: color.grey.A400,
+});
+
+globalStyle('*::-webkit-scrollbar-track', {
+  backgroundColor: 'transparent',
 });
 
 globalStyle('body', {
