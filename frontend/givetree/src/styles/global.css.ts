5233001a54ab3cdbc3ac76295919f3141e98e0fc
= <kimdw524@naver.com>
2024-11-03 03:48:03 +0900

    fe/feat: scrollbar 제거

--- frontend/givetree/src/styles/global.css.ts
+++ frontend/givetree/src/styles/global.css.ts
@@ -1,28 +1,18 @@
 import { globalStyle } from '@vanilla-extract/css';
 
 import colorPalette from '@/styles/tokens/colorPalette';
-import color from '@/styles/tokens/color';
 
 globalStyle('*', {
   margin: '0',
   boxSizing: 'border-box',
-  overflow: 'auto',
+  overflow: 'overlay',
   fontFamily: 'inherit',
   WebkitTapHighlightColor: 'transparent',
 });
 
 globalStyle('*::-webkit-scrollbar', {
-  width: '0.25rem',
-  height: '0.25rem',
-});
-
-globalStyle('*::-webkit-scrollbar-thumb', {
-  borderRadius: '0.75rem',
-  backgroundColor: color.grey.A400,
-});
-
-globalStyle('*::-webkit-scrollbar-track', {
-  backgroundColor: 'transparent',
+  width: '0',
+  height: '0',
 });
 
 globalStyle('body', {
