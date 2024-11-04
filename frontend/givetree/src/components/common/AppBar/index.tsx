0619c7a8d4cdb2db2cc5df4c524f5499496b0882
kimdw524 <kimdw524@naver.com>
2024-11-04 11:47:09 +0900

    fe/feat: AppBar 추가
    Former-commit-id: 3e2ad22f1581f4448ef04532d180439c185f7297

--- frontend/givetree/src/components/common/AppBar/index.tsx
+++ frontend/givetree/src/components/common/AppBar/index.tsx
@@ -1,21 +1,4 @@
-import { ReactNode } from 'react';
+import AppBar from '@/components/common/AppBar/AppBar';
+import AppBarMenu from '@/components/common/AppBar/AppBarMenu';
 
-import * as s from './AppBar.css';
-
-import Typography from '@/components/common/Typography';
-
-interface AppBarProps {
-  children: ReactNode;
-}
-
-const AppBar = ({ children }: AppBarProps) => {
-  return (
-    <nav className={s.container}>
-      <Typography as="h1" color="#fff" weight="light">
-        {children}
-      </Typography>
-    </nav>
-  );
-};
-
-export default AppBar;
+export default Object.assign(AppBar, { Menu: AppBarMenu });
