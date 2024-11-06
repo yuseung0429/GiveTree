79e8d5e8b82dbb694549f0b9a2158356c78ddd71
kimdw524 <kimdw524@naver.com>
2024-11-06 15:39:11 +0900

    fe/feat: Typography ellipsis 추가

--- frontend/givetree/src/components/market/SearchItem/index.tsx
+++ frontend/givetree/src/components/market/SearchItem/index.tsx
@@ -1,5 +1,7 @@
 import * as s from './SearchItem.css';
 
+import Link from 'next/link';
+
 import typography from '@/styles/tokens/typography';
 
 import Box from '@/components/common/Box';
@@ -10,46 +12,41 @@ import Chip from '@/components/common/Chip';
 const SearchItem = () => {
   return (
     <div className={s.wrapper}>
-      <Flex gap="0.5rem" alignItems="center" className={s.container}>
-        <Box
-          padding="1rem"
-          borderRadius="0.75rem"
-          backgroundColor="#eee"
-          width="96px"
-          height="96px"
-          style={{ flex: '0 0 auto' }}
-        ></Box>
-        <Box style={{ flex: '1 1 auto', overflow: 'hidden' }}>
-          <Typography
-            weight="medium"
-            style={{
-              overflow: 'hidden',
-              whiteSpace: 'nowrap',
-              textOverflow: 'ellipsis',
-            }}
-          >
-            갤럭시s23울트라1tb 팬텀블랙 자급제
-          </Typography>
-          <Typography
-            size={typography.size.sm}
-            weight="regular"
-            style={{ margin: '0.25rem 0' }}
-          >
-            3시간 전
-          </Typography>
-          <Typography
-            size={typography.size.lg}
-            weight="semiBold"
-            style={{ margin: '0.5rem 0' }}
-          >
-            850,000원
-          </Typography>
-          <Flex gap="0.375rem">
-            <Chip size="sm">판매중</Chip>
-            <Chip size="sm">직거래</Chip>
-          </Flex>
-        </Box>
-      </Flex>
+      <Link href="/market/detail">
+        <Flex gap="0.5rem" alignItems="center" className={s.container}>
+          <Box
+            padding="1rem"
+            borderRadius="0.75rem"
+            backgroundColor="#eee"
+            width="96px"
+            height="96px"
+            style={{ flex: '0 0 auto' }}
+          ></Box>
+          <Box style={{ flex: '1 1 auto', overflow: 'hidden' }}>
+            <Typography weight="medium" ellipsis>
+              갤럭시s23울트라1tb 팬텀블랙 자급제
+            </Typography>
+            <Typography
+              size={typography.size.sm}
+              weight="regular"
+              style={{ margin: '0.25rem 0' }}
+            >
+              3시간 전
+            </Typography>
+            <Typography
+              size={typography.size.lg}
+              weight="semiBold"
+              style={{ margin: '0.5rem 0' }}
+            >
+              850,000원
+            </Typography>
+            <Flex gap="0.375rem">
+              <Chip size="sm">판매중</Chip>
+              <Chip size="sm">직거래</Chip>
+            </Flex>
+          </Box>
+        </Flex>
+      </Link>
     </div>
   );
 };
