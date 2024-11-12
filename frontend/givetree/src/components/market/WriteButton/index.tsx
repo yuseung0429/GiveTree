ce1c244e3b19a6f710e82f5ab2b967f11dc7b201
kimdw524 <kimdw524@naver.com>
2024-11-12 12:54:34 +0900

    fe/feat: WriteButton link 추가

--- frontend/givetree/src/components/market/WriteButton/index.tsx
+++ frontend/givetree/src/components/market/WriteButton/index.tsx
@@ -2,6 +2,8 @@
 
 import { useRef } from 'react';
 
+import Link from 'next/link';
+
 import { HiPencilSquare } from 'react-icons/hi2';
 
 import useIsScrolled from '@/hooks/useIsScrolled';
@@ -11,21 +13,24 @@ import Button from '@/components/common/Button';
 import * as s from './WriteButton.css';
 
 interface WriteButtonProps {
+  href: string;
   onClick?: () => void;
 }
 
-const WriteButon = ({ onClick }: WriteButtonProps) => {
+const WriteButon = ({ href, onClick }: WriteButtonProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const isScrolled = useIsScrolled(containerRef);
 
   return (
     <div className={s.container} ref={containerRef}>
-      <Button
-        className={isScrolled ? s.rounded : s.rectangle}
-        onClick={onClick}
-      >
-        {isScrolled ? <HiPencilSquare size="1.25rem" /> : '글쓰기'}
-      </Button>
+      <Link href={href}>
+        <Button
+          className={isScrolled ? s.rounded : s.rectangle}
+          onClick={onClick}
+        >
+          {isScrolled ? <HiPencilSquare size="1.25rem" /> : '글쓰기'}
+        </Button>
+      </Link>
     </div>
   );
 };
