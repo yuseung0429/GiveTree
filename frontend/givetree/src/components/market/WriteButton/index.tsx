1a091f9e071d91f7ae79870284836f9f843dcf92
kimdw524 <kimdw524@naver.com>
2024-11-12 11:01:19 +0900

    fe/feat: 거래 글쓰기 버튼 추가

---
+++ frontend/givetree/src/components/market/WriteButton/index.tsx
@@ -0,0 +1,31 @@
+import { useRef } from 'react';
+
+import { HiPencilSquare } from 'react-icons/hi2';
+
+import useIsScrolled from '@/hooks/useIsScrolled';
+
+import Button from '@/components/common/Button';
+
+import * as s from './WriteButton.css';
+
+interface WriteButtonProps {
+  onClick: () => void;
+}
+
+const WriteButon = ({ onClick }: WriteButtonProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const isScrolled = useIsScrolled(containerRef);
+
+  return (
+    <div className={s.container} ref={containerRef}>
+      <Button
+        className={isScrolled ? s.rounded : s.rectangle}
+        onClick={onClick}
+      >
+        {isScrolled ? <HiPencilSquare size="1.25rem" /> : '글쓰기'}
+      </Button>
+    </div>
+  );
+};
+
+export default WriteButon;
