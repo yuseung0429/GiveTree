0bc197d658d418cb61d9ce13d30b7eb8637ca3bd
kimdw524 <kimdw524@naver.com>
2024-11-12 14:04:22 +0900

    fe/feat: viewport 확대 방지

--- frontend/givetree/src/app/layout.tsx
+++ frontend/givetree/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import localFont from 'next/font/local';
 
 import '@/styles/global.css';
@@ -13,6 +13,13 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: 'GiveTree',
   description: 'GiveTree',
+};
+
+export const viewport: Viewport = {
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  minimumScale: 1.0,
+  userScalable: false,
 };
 
 export default function RootLayout({
