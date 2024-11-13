2d4fdde3513d2cf4aed61506d503a4f8280b0977
dbe0717 <dbe0717@naver.com>
2024-11-13 12:43:26 +0900

    fe/feat: build error solve

--- frontend/givetree/src/app/foundation/all/layout.tsx
+++ frontend/givetree/src/app/foundation/all/layout.tsx
@@ -1,21 +1,16 @@
-'use client';
-
 import AppBar from '@/components/common/AppBar';
 import Layout from '@/components/common/Layout';
 import NavigationBar from '@/components/common/NavigationBar';
-import { useRouter } from 'next/navigation';
 
 export default function FoundationLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-
   return (
     <Layout>
       <header>
-        <AppBar title="재단 전체보기" onBackClick={() => router.back()} />
+        <AppBar title="재단 전체보기" />
       </header>
       <main>{children}</main>
       <footer>
