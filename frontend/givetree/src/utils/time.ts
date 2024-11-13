3f94474209bcaf875e69ef14db6d9554737a8e46
kimdw524 <kimdw524@naver.com>
2024-11-13 13:33:47 +0900

    fe/feat: 시간 formatting 함수

--- frontend/givetree/src/utils/time.ts
+++ frontend/givetree/src/utils/time.ts
@@ -18,3 +18,20 @@ export const getTimeDifference = (time: string) => {
 
   return `${Math.floor(difference / 3600 / 24)}일 전`;
 };
+
+export const formatTime = (time: string) => {
+  const date = new Date(time);
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  const formattedMonth = month < 10 ? '0' + month : month;
+  const formattedDate = day < 10 ? '0' + day : day;
+  const formattedHours = hours < 10 ? '0' + hours : hours;
+  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+  return `${year}. ${formattedMonth}. ${formattedDate} ${formattedHours}:${formattedMinutes}`;
+};
