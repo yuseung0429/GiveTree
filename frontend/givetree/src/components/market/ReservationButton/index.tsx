d703e4da9c08d56930080693f015fe74758ef800
kimdw524 <kimdw524@naver.com>
2024-11-15 01:30:17 +0900

    fe/feat: 예약 및 예약 취소 임시 구현

--- frontend/givetree/src/components/market/ReservationButton/index.tsx
+++ frontend/givetree/src/components/market/ReservationButton/index.tsx
@@ -2,6 +2,9 @@
 
 import useIsReservedPurchaser from '@/api/market/useIsReservedPurchaser';
 
+import reserveSale from '@/actions/market/reserveSale';
+import cancelReserve from '@/actions/market/cancelReserve';
+
 import useDialog from '@/hooks/useDialog';
 
 import Button from '@/components/common/Button';
@@ -17,13 +20,17 @@ const ReservationButton = ({ saleId, purchaserId }: ReservationButtonProps) => {
 
   const handleReserveClick = async () => {
     if (await confirm('상대방에게 상품 판매를 예약하시겠습니까?')) {
-      alert('완료');
+      if (await reserveSale(saleId, purchaserId)) {
+        alert('예약되었습니다.');
+      }
     }
   };
 
   const handleCancelClick = async () => {
     if (await confirm('상품 판매 예약을 취소하시겠습니까?')) {
-      alert('완료');
+      if (await cancelReserve(saleId)) {
+        alert('취소되었습니다.');
+      }
     }
   };
 
