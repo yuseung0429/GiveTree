f759d1fb4824aa1c5277a5224603d0835b2e9cb4
kimdw524 <kimdw524@naver.com>
2024-11-14 18:00:09 +0900

    fe/feat: modal transition animation 레이아웃에서 분리

--- frontend/givetree/src/app/market/(page)/(main)/layout.tsx
+++ frontend/givetree/src/app/market/(page)/(main)/layout.tsx
@@ -1,19 +1,11 @@
-'use client';
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-
-import { AnimatePresence, motion } from 'framer-motion';
 
 import { HiMagnifyingGlass, HiOutlineBell } from 'react-icons/hi2';
 
-import * as s from './@modal/Modal.css';
-
-import { mergeClasses } from '@/utils/mergeClasses';
-
+import MarketAnimation from './animation';
 import AppBar from '@/components/common/AppBar';
-import FrozenRouter from '@/components/common/FrozenRouter';
 import Layout from '@/components/common/Layout';
+import NavigationBar from '@/components/common/NavigationBar';
 
 export default function MarketLayout({
   children,
@@ -22,9 +14,6 @@ export default function MarketLayout({
   children: React.ReactNode;
   modal: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const exceptions = ['/notification'];
-
   return (
     <Layout>
       <header>
@@ -43,27 +32,11 @@ export default function MarketLayout({
       </header>
       <main>
         <section style={{ height: '100%' }}>{children}</section>
-        {/* 리팩토링 필요 */}
-        {exceptions.includes(pathname) ? (
-          <>{modal}</>
-        ) : (
-          <section>
-            <AnimatePresence mode="sync" initial={false}>
-              <motion.div
-                key={pathname === '/market' ? 'market' : 'modal'}
-                initial={{ y: '5%', opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: '5%', opacity: 0 }}
-                transition={{ duration: 0.2, ease: 'easeInOut' }}
-                className={mergeClasses(pathname !== '/market' && s.modal)}
-              >
-                <FrozenRouter key={pathname}>{modal}</FrozenRouter>
-              </motion.div>
-            </AnimatePresence>
-          </section>
-        )}
+        <MarketAnimation>{modal}</MarketAnimation>
       </main>
-      <footer></footer>
+      <footer>
+        <NavigationBar />
+      </footer>
     </Layout>
   );
 }
