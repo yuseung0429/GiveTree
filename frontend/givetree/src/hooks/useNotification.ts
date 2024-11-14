588cdf34c0d25aaa2a2a834d9787b400b76b9e93
kimdw524 <kimdw524@naver.com>
2024-11-15 03:07:26 +0900

    fe/fix: serviceWorker URL이 아니라 scope로 변경

--- frontend/givetree/src/hooks/useNotification.ts
+++ frontend/givetree/src/hooks/useNotification.ts
@@ -21,7 +21,7 @@ const useNotification = () => {
 
   useEffect(() => {
     Notification.requestPermission().then((permission) => {
-      const serviceWorkerURL = '/firebase-messaging-sw.js';
+      const serviceWorkerScope = '/firebase-cloud-messaging-push-scope';
       setPermission(permission);
 
       if (permission !== 'granted') {
@@ -32,8 +32,8 @@ const useNotification = () => {
 
       const messaging = getMessaging();
 
-      navigator.serviceWorker.register(serviceWorkerURL, {
-        scope: '/firebase-cloud-messaging-push-scope',
+      navigator.serviceWorker.register('/firebase-messaging-sw.js', {
+        scope: serviceWorkerScope,
       });
 
       getToken(messaging, {
@@ -50,7 +50,7 @@ const useNotification = () => {
         const { title, body, image } = payload.notification!;
 
         const registration = await navigator.serviceWorker.getRegistration(
-          serviceWorkerURL
+          serviceWorkerScope
         );
 
         registration?.showNotification(title || '', {
