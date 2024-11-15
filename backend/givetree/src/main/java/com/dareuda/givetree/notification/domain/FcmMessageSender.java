19a592b6f07686305d756ceb2122f6d4af8cfbac
권용진 <dydwls9703@naver.com>
2024-11-15 18:00:01 +0900

    be/wip: 중간 저장
    #116

--- backend/givetree/src/main/java/com/dareuda/givetree/notification/domain/FcmMessageSender.java
+++ backend/givetree/src/main/java/com/dareuda/givetree/notification/domain/FcmMessageSender.java
@@ -12,14 +12,12 @@ public class FcmMessageSender {
     private final MemberFcmTokenReader memberFcmTokenReader;
     private final FcmCaller fcmCaller;
 
-    @Async
     public void send(long memberId, String title, String body) {
         MemberFcmToken memberFcmToken = memberFcmTokenReader.read(memberId);
         String jsonFcmMessage = FcmMessageWriter.write(memberFcmToken.getToken(), Notification.of(title, body));
         fcmCaller.call(jsonFcmMessage);
     }
 
-    @Async
     public void sendTest(String token, Notification notification) {
         String jsonFcmMessage = FcmMessageWriter.write(token, notification);
         fcmCaller.call(jsonFcmMessage);
