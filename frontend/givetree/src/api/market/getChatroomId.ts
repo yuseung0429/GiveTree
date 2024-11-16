02762147d6f6a385d8849cae272184561da2e3e4
kimdw524 <kimdw524@naver.com>
2024-11-16 15:22:29 +0900

    fe/fix: console.log 제거

--- frontend/givetree/src/api/market/getChatroomId.ts
+++ frontend/givetree/src/api/market/getChatroomId.ts
@@ -1,13 +1,11 @@
 import { axiosInstance } from '@/api/axiosInstance';
 
 const getChatroomId = async (saleId: number) => {
-  console.log('saleId: ', saleId);
   const response = await axiosInstance.post(`/chatrooms/connect`, { saleId });
 
   if (response.status !== 200) {
     throw new Error('존재하지 않는 채팅방입니다.');
   }
-  console.log(response);
 
   const data: { chatroomId: number } = response.data;
   return data.chatroomId;
