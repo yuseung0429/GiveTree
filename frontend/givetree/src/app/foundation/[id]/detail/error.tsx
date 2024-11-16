0fc216fe1bf043df5b42e2ed19c12e0a272e3615
kimdw524 <kimdw524@naver.com>
2024-11-16 16:48:51 +0900

    fe/feat: 재단 에러 핸들링

--- frontend/givetree/src/app/foundation/[id]/detail/error.tsx
+++ frontend/givetree/src/app/foundation/[id]/detail/error.tsx
@@ -1,5 +1,32 @@
 'use client';
 
-export default function FoundationErrror() {
-  return <>에러가 발생함</>;
+import { useRouter } from 'next/navigation';
+
+import Button from '@/components/common/Button';
+import Flex from '@/components/common/Flex';
+import Typography from '@/components/common/Typography';
+
+export default function FoundationError() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.back();
+  };
+
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      gap="0.75rem"
+      height="100%"
+    >
+      <Typography style={{ textAlign: 'center' }}>
+        재단 정보를 불러오지 못했습니다.
+      </Typography>
+      <Button color="secondary" onClick={handleClick}>
+        뒤로가기
+      </Button>
+    </Flex>
+  );
 }
