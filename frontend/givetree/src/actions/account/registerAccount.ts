9984b6d7af7b8e81d41f44a89d721c09ce450994
kimdw524 <kimdw524@naver.com>
2024-11-16 15:27:45 +0900

    fe/fix: resolve eslint error

--- frontend/givetree/src/actions/account/registerAccount.ts
+++ frontend/givetree/src/actions/account/registerAccount.ts
@@ -14,8 +14,6 @@ export async function registerAccount(accountNumber: string) {
       }),
     });
 
-    const data = await response.text();
-
     if (response.ok) {
       return { success: true };
     }
