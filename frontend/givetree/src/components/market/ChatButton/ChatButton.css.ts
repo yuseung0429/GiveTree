d4dc92b59bbf46b3b92e43d0983996b243f9436f
kimdw524 <kimdw524@naver.com>
2024-11-16 15:21:40 +0900

    fe/feat: 채팅방 목록 보기 추가

---
+++ frontend/givetree/src/components/market/ChatButton/ChatButton.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  position: 'absolute',
+  left: '1rem',
+  bottom: '1rem',
+});
+
+export const rounded = style({
+  width: '2.75rem',
+  padding: '0 !important',
+});
