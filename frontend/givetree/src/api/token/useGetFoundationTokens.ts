6f81c85284c5eacb6bb64c73168658fab987cb07
kimdw524 <kimdw524@naver.com>
2024-11-17 23:58:27 +0900

    fe/feat: 토큰 조회  dedupingInterval 추가

--- frontend/givetree/src/api/token/useGetFoundationTokens.ts
+++ frontend/givetree/src/api/token/useGetFoundationTokens.ts
@@ -5,7 +5,9 @@ import useSWR from 'swr';
 import { TokenListResult } from '@/types/token/token';
 
 const useGetFoundationTokenList = () => {
-  const { data } = useSWR<TokenListResult>('/tokens/donations/foundations');
+  const { data } = useSWR<TokenListResult>('/tokens/donations/foundations', {
+    dedupingInterval: 3000,
+  });
   return data;
 };
 
