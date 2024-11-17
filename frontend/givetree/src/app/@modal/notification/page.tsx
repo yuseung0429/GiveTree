6d34f32775733c381cf5df39463483ae2b917cd4
kimdw524 <kimdw524@naver.com>
2024-11-18 01:16:57 +0900

    fe/feat: 알림 없을 때 페이지 추가

--- frontend/givetree/src/app/@modal/notification/page.tsx
+++ frontend/givetree/src/app/@modal/notification/page.tsx
@@ -1,6 +1,7 @@
 import getNotifications from '@/api/notification/getNotifications';
 
 import Box from '@/components/common/Box';
+import NoNotification from '@/components/notification/NoNotification';
 import NotificationItem from '@/components/notification/NotificationItem';
 
 export default async function Notification() {
@@ -8,9 +9,11 @@ export default async function Notification() {
 
   return (
     <Box height="100%" padding="0.5rem" style={{ overflow: 'scroll' }}>
-      {data.map((item, index) => (
-        <NotificationItem key={index} {...item} />
-      ))}
+      {data.length === 0 ? (
+        <NoNotification />
+      ) : (
+        data.map((item, index) => <NotificationItem key={index} {...item} />)
+      )}
     </Box>
   );
 }
