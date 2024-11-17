e3e9bc47798c9879ce19c468c239b653ebc840bd
kimdw524 <kimdw524@naver.com>
2024-11-18 00:28:55 +0900

    fe/feat: 상품 결제 API 연동

---
+++ frontend/givetree/src/actions/market/payItem.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import fetchWrapper from '@/lib/fetchWrapper';
+
+export default async function payItem(
+  saleId: number,
+  simplePassword: string
+): Promise<{ result: boolean; message?: string }> {
+  try {
+    const response = await fetchWrapper(`/sales/${saleId}/pay`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        simplePassword,
+      }),
+    });
+
+    return {
+      result: response.status === 200,
+      message: (await response.json()).message,
+    };
+  } catch {
+    return { result: false, message: '알 수 없는 오류가 발생하였습니다.' };
+  }
+}
