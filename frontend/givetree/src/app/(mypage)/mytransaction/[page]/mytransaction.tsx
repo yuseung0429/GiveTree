dc6b3ff7a4a6c1caaf8b3453ada7dcf3cf873737
kimdw524 <kimdw524@naver.com>
2024-11-18 03:13:06 +0900

    fe/feat: 입출금 내역 없을 때 표시 페이지

--- frontend/givetree/src/app/(mypage)/mytransaction/[page]/mytransaction.tsx
+++ frontend/givetree/src/app/(mypage)/mytransaction/[page]/mytransaction.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import { HiCloud } from 'react-icons/hi2';
+
 import { getTransaction } from '@/api/ledger/getTransaction';
 
 import Box from '@/components/common/Box';
@@ -12,35 +14,46 @@ export default async function MyTransaction({ page }: { page: number }) {
   const data = await getTransaction(page);
 
   return (
-    <Box padding="1rem">
-      <Flex flexDirection="column" gap="1rem">
-        {data.content.map((item, index) => (
-          <ExpenseItem
-            key={index}
-            amount={item.amount}
-            date={item.processedAt}
-            message={item.message}
-            type={item.type}
-          />
-        ))}
-      </Flex>
-      <Flex
-        alignItems="center"
-        justifyContent="space-between"
-        style={{ marginTop: '1rem' }}
-      >
-        <Link href={`/mytransaction/${data.number - 1}`}>
-          <Button size="sm" disabled={!data.number}>
-            이전
-          </Button>
-        </Link>
-        <Typography>{data.number + 1}</Typography>
-        <Link href={`/mytransaction/${data.number + 1}`}>
-          <Button size="sm" disabled={data.number >= data.totalPages - 1}>
-            다음
-          </Button>
-        </Link>
-      </Flex>
+    <Box height="100%" padding="1rem">
+      {data.content.length === 0 ? (
+        <Flex alignItems="center" justifyContent="center" height="100%">
+          <Flex flexDirection="column" alignItems="center" gap="1rem">
+            <HiCloud size="3rem" />
+            <Typography>입출금 내역이 없습니다.</Typography>
+          </Flex>
+        </Flex>
+      ) : (
+        <>
+          <Flex flexDirection="column" gap="1rem">
+            {data.content.map((item, index) => (
+              <ExpenseItem
+                key={index}
+                amount={item.amount}
+                date={item.processedAt}
+                message={item.message}
+                type={item.type}
+              />
+            ))}
+          </Flex>
+          <Flex
+            alignItems="center"
+            justifyContent="space-between"
+            style={{ marginTop: '1rem' }}
+          >
+            <Link href={`/mytransaction/${data.number - 1}`}>
+              <Button size="sm" disabled={!data.number}>
+                이전
+              </Button>
+            </Link>
+            <Typography>{data.number + 1}</Typography>
+            <Link href={`/mytransaction/${data.number + 1}`}>
+              <Button size="sm" disabled={data.number >= data.totalPages - 1}>
+                다음
+              </Button>
+            </Link>
+          </Flex>
+        </>
+      )}
     </Box>
   );
 }
