7c392b949bea4b69ff76386d1ac08acf6766d299
kimdw524 <kimdw524@naver.com>
2024-11-18 17:36:53 +0900

    fe/feat: 알림 ErrorBoundary 추가

--- frontend/givetree/src/app/@modal/notification/page.tsx
+++ frontend/givetree/src/app/@modal/notification/page.tsx
@@ -1,46 +1,13 @@
-import { HiMiniBell } from 'react-icons/hi2';
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 
-import getNotifications from '@/api/notification/getNotifications';
+import NotificationError from './error';
+import Notification from './notification';
 
-import colorPalette from '@/styles/tokens/colorPalette';
-
-import Box from '@/components/common/Box';
-import Flex from '@/components/common/Flex';
-import Typography from '@/components/common/Typography';
-import NoNotification from '@/components/notification/NoNotification';
-import NotificationItem from '@/components/notification/NotificationItem';
-
-export default async function Notification() {
-  const data = await getNotifications();
-
+export default async function NotificationPage() {
   return (
-    <Box height="100%" padding="0.75rem" style={{ overflow: 'scroll' }}>
-      <Flex flexDirection="column" height="100%">
-        <Box paddingBottom="0.75rem" style={{ flex: '0 0 auto' }}>
-          <Typography
-            color={colorPalette.primary[700]}
-            weight="semiBold"
-            style={{ display: 'flex', alignItems: 'center', gap: '0.25rem' }}
-          >
-            <HiMiniBell size="1.5rem" />
-            알림
-          </Typography>
-        </Box>
-        <Flex
-          flexDirection="column"
-          gap="0.75rem"
-          style={{ flex: '1 1 auto', overflow: 'scroll' }}
-        >
-          {data.length === 0 ? (
-            <NoNotification />
-          ) : (
-            data.map((item, index) => (
-              <NotificationItem key={index} {...item} />
-            ))
-          )}
-        </Flex>
-      </Flex>
-    </Box>
+    <ErrorBoundary errorComponent={NotificationError}>
+      <Notification />;
+    </ErrorBoundary>
   );
 }
 
