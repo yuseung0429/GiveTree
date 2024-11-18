789482efc474941cd524f6359fd1599ca3a4f34a
신재민 <woals1488@naver.com>
2024-11-18 13:53:23 +0900

    be/fix: WebSocket CORS 설정 변경

--- backend/givetree/src/main/java/com/dareuda/givetree/common/config/WebSocketConfig.java
+++ backend/givetree/src/main/java/com/dareuda/givetree/common/config/WebSocketConfig.java
@@ -12,6 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/ws")
+                .setAllowedOrigins("https://givetree.co.kr")
                 .withSockJS();
     }
 
